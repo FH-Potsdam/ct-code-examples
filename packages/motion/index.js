aac178904f0f20d65de2781dcaf2d8dbf0333695
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-11-07 09:43:54 +0100

    device motion seems broken

--- packages/motion/index.js
+++ packages/motion/index.js
@@ -1,14 +1,15 @@
 /**
  * Using devicde motion seems to be a hassle.
- * Only made it work on iOS FireFox by using a selfsigned certificate
+ * Only made it work on Brave on iOS by using a selfsigned certificate
  * See https://stackoverflow.com/questions/35127383/npm-http-server-with-ssl
  * See https://web.archive.org/web/20190114150423/https://docs.nodejitsu.com/articles/HTTP/servers/how-to-create-a-HTTPS-server/
  *
  *
  */
-let x = 0;
+
 let canvas = undefined;
 let ax, ay, az;
+let ball = undefined;
 function handleMotionEvent(event) {
   ax = event.accelerationIncludingGravity.x;
   ay = event.accelerationIncludingGravity.y;
@@ -18,12 +19,17 @@ function handleMotionEvent(event) {
 function setup() {
   canvas = createCanvas(300, 300);
   canvas.parent("sketch");
+  ball = new Ball(width / 2, height / 2);
   // window.addEventListener("devicemotion", handleMotionEvent, true);
 }
 
 function draw() {
   background(255);
-  text(`a x: ${ax} y: ${ay} z: ${az}`, 10, 10);
+  if (ax !== undefined && ay !== undefined && az !== undefined) {
+    console.log(ax, ay, az);
+    ball.update(ball.x + ax, ball.y + ay * -1);
+    ball.display();
+  }
 }
 
 function keyPressed() {
@@ -35,20 +41,46 @@ function keyPressed() {
   }
 }
 
+function Ball(_x, _y) {
+  if (!(this instanceof Ball)) {
+    throw new TypeError(
+      'Ball can not be called as a function. Call it like this "const ball = new Ball(x,y)"',
+    );
+  }
+  this.x = _x;
+  this.y = _y;
+  this.display = function() {
+    fill(255);
+    ellipse(this.x, this.y, 10, 10);
+  };
+  this.update = function(__x, __y) {
+    this.x = __x;
+    this.y = __y;
+    if (this.x < 0) this.x = 0;
+    if (this.x > width) this.x = width;
+    if (this.y < 0) this.y = 0;
+    if (this.y > height) this.y = height;
+  };
+}
 /**
  * Make it work on safari
  */
-document.body.onclick = function onClick() {
-  // feature detect
-  if (typeof DeviceMotionEvent.requestPermission === "function") {
-    DeviceMotionEvent.requestPermission()
-      .then((permissionState) => {
-        if (permissionState === "granted") {
-          window.addEventListener("devicemotion", handleMotionEvent, true);
-        }
-      })
-      .catch(console.error);
-  } else {
-    // handle regular non iOS 13+ devices
-  }
-};
+const button = document.querySelector("button#allow");
+if (button === undefined) {
+  throw new Error("button does not exist");
+} else {
+  button.onclick = function onClick() {
+    // feature detect
+    if (typeof DeviceMotionEvent.requestPermission === "function") {
+      DeviceMotionEvent.requestPermission()
+        .then((permissionState) => {
+          if (permissionState === "granted") {
+            window.addEventListener("devicemotion", handleMotionEvent, true);
+          }
+        })
+        .catch(console.error);
+    } else {
+      // handle regular non iOS 13+ devices
+    }
+  };
+}
